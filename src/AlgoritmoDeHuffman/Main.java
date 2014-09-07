dc5f81c74503a5b97ddfdd1301b3caa83d9dc84a
gar13177 <gar13177@uvg.edu.gt>
2014-09-07 15:15:10 -0600

    Main terminado, solo falta generar tabla de codigos

--- src/AlgoritmoDeHuffman/Main.java
+++ src/AlgoritmoDeHuffman/Main.java
@@ -10,6 +10,9 @@
 
 package AlgoritmoDeHuffman;
 
+import java.util.Scanner;
+import java.util.*;
+
 /**
  *
  * @author Kevin
@@ -22,14 +25,43 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        ArbolBinario _newArbol = new ArbolBinario();
-
-        System.out.println(_newArbol.Preorden());
-        System.out.println(_newArbol.Postorden());
-        System.out.println(_newArbol.Inorden());
+        Scanner in  = new Scanner(System.in);
+        TreeSet _CaracIng = new TreeSet(new OrdenarPorOcurrencia() );
+        OrdenamientoHuffman _orden = new OrdenamientoHuffman();
         
-        System.out.println(_newArbol.Contar());
-        System.out.println(_newArbol.Altura());
+        System.out.println("Ingrese la cadena a codificar");
+        String mensaje = in.nextLine();
+        
+        while (mensaje.length() != 0){
+            _CaracIng.add(new NodoHuffman(""+mensaje.charAt(mensaje.length()-1),mensaje.length() - mensaje.replace(""+mensaje.charAt(mensaje.length()-1), "").length()));
+            mensaje = mensaje.replace(""+mensaje.charAt(mensaje.length()-1), "");
+        }
+        System.out.println(_CaracIng);  
+        
+        
+        while (_CaracIng.size() != 1){
+             
+            NodoHuffman tmp1 = (NodoHuffman)_CaracIng.first();
+            _CaracIng.pollFirst();
+            NodoHuffman tmp2 = (NodoHuffman)_CaracIng.first();
+            _CaracIng.pollFirst();
+            
+            tmp1.setCodigo("0"+tmp1.getCodigo());
+            tmp2.setCodigo("1"+tmp2.getCodigo());
+            
+            
+            NodoHuffman tmp3 = new NodoHuffman(tmp1.getDato()+tmp2.getDato(),tmp1.getOcurrencia()+tmp2.getOcurrencia());
+            tmp3.setHijoIzq(tmp1);
+            tmp3.setHijoDer(tmp2);
+            
+            _CaracIng.add(tmp3);
+            System.out.println(_CaracIng); 
+          
+        }
+        
+        ArbolHuffman _newArbol = new ArbolHuffman((NodoHuffman)_CaracIng.first());
+        //System.out.println(_newArbol.Preorden());
+
     }
     
 }
