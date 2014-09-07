92ce7a5c5499c6258cb56b82d27e913169e66b6b
gar13177 <gar13177@uvg.edu.gt>
2014-09-07 00:05:37 -0600

    Hoja de trabajo 7 comenzada

---
+++ src/AlgoritmoDeHuffman/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos 2014
+ * Hoja de Trabajo No. 7
+ * Algoritmo de Huffman
+ * 
+ * Vidal Villegas Zabala 13023
+ * Kevin Estuardo Garcia 13177
+ */
+
+package AlgoritmoDeHuffman;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
