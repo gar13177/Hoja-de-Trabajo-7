c524830d5774ac1374c0c9d71b48029c39fb7fa7
gar13177 <gar13177@uvg.edu.gt>
2014-09-07 02:40:02 -0600

    Main de prueba completado

--- src/AlgoritmoDeHuffman/Main.java
+++ src/AlgoritmoDeHuffman/Main.java
@@ -21,6 +21,33 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        ArbolBinario _newArbol = new ArbolBinario();
+        
+        NodoBinario n1 = new NodoBinario("1");
+        NodoBinario n2 = new NodoBinario("2");
+        NodoBinario n3 = new NodoBinario("3");
+        NodoBinario n4 = new NodoBinario("4");
+        NodoBinario n5 = new NodoBinario("5");
+        NodoBinario n6 = new NodoBinario("6");
+        NodoBinario n7 = new NodoBinario("7");
+        
+        n5.setHijoIzq(n6);
+        n5.setHijoDer(n7);
+        n2.setHijoIzq(n4);
+        n2.setHijoDer(n5);
+        n1.setHijoIzq(n2);
+        n1.setHijoDer(n3);
+        _newArbol.setRaiz(n1);
+        
+        
+        
+        System.out.println(_newArbol.Preorden());
+        System.out.println(_newArbol.Postorden());
+        System.out.println(_newArbol.Inorden());
+        
+        System.out.println(_newArbol.Contar());
+        System.out.println(_newArbol.Altura());
     }
     
 }
